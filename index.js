47f442a8e3fe8e582585c0a1ecae1627625534bd
Marybethy <marybethsoriano67@gmail.com>
2024-07-03 23:37:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         price4: 10000.00,
         price5: 3500.00,
         price6: 13000.00,
-        price7: 10000.00,
     };
 
     const qtyInputs = [
@@ -16,7 +15,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         document.getElementById('qty4'),
         document.getElementById('qty5'),
         document.getElementById('qty6'),
-        document.getElementById('qty7')
     ];
 
     const totalInput = document.getElementById('total');
